b341be39d3de4e8619922d59cb20f5bcc9f993e4
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2024-03-22 20:46:39 +0300

    update

--- app.js
+++ app.js
@@ -26,38 +26,6 @@ const database = "ShopDb";
 
 })()
 
-const productSchema = mongoose.Schema({
-    name: String,
-    price: Number,
-    description: String,
-    imageUrl: String,
-    date: {
-        type: Date,
-        default: Date.now
-    },
-    isActive: Boolean
-})
-
-const Product = mongoose.model("Product", productSchema) //model
-const prd = new Product({
-    name: "iPhone 14 Pro",
-    price: 60.000,
-    description: "İyi Telefon",
-    imageUrl: "1.jpeg",
-    isActive: true
-})
-async function saveProduct() {
-    try {
-        await prd.save()
-    }
-    catch (err) {
-        console.log(err)
-    }
-}
-
-saveProduct()
-
-
 app.listen(3000, () => {
     console.log("listening on port 3000")
 })
--- routes/products.js
+++ routes/products.js
@@ -2,31 +2,40 @@ const express = require("express")
 const router = express.Router()
 const Joi = require("joi")
 
+const { Product , validateProduct} = require("../models/product")
+
 const products = [
-    { id: 1, name: "iphone 12", price: 20000 },
-    { id: 2, name: "iphone 13", price: 30000 },
-    { id: 3, name: "iphone 14", price: 40000 }
+    {id: 1, name: "iphone 12", price:20000},
+    {id: 2, name: "iphone 13", price:30000},
+    {id: 3, name: "iphone 14", price:40000},
 ]
 
 router.get("/", (req, res) => {
     res.send(products)
 })
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
     const { error } = validateProduct(req.body)
 
     if (error) {
-        return res.status(400).send(result.error.details[0].message)
+        return res.status(400).send(error.details[0].message)
 
     }
 
-    const product = {
-        id: products.length + 1,
+    const product = new Product({
         name: req.body.name,
-        price: req.body.price
+        price: req.body.price,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        isActive: req.body.isActive
+    })
+
+    try {
+        await product.save()
     }
-    products.push(product)
-    res.send(product)
+    catch (err) {
+        console.log(err)
+    }
 })
 
 router.put("/:id", (req, res) => {
@@ -69,12 +78,5 @@ router.get("/:id", (req, res) => {
     }
 })
 
-function validateProduct(product) {
-    const schema = new Joi.object({
-        name: Joi.string().min(3).max(30).required(),
-        price: Joi.number().required().min(0)
-    })
 
-    return result = schema.validate(product)
-}
 module.exports = router
