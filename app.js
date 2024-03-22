fba94c22772206451d3608563872c7d737ef6e26
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2024-03-22 17:22:35 +0300

    update

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 
+app.use(express.json())
+
 const products = [
     {id:1, name: "iphone 12", price: 20000},
     {id:2, name: "iphone 13", price: 30000},
@@ -19,7 +21,18 @@ app.get("/api/products", (req,res) => {
     res.send(products)
 })
 
-app.post("/api/products")
+app.post("/api/products", (req,res) => {
+    if(!req.body.name || req.body.name.length <= 3){
+        res.status(400).send("Ürün adı bilgisini en az 3 karakter olarak girmelisiniz.")
+    }
+    const product = {
+        id: products.length + 1,
+        name: req.body.name,
+        price:  req.body.price
+    }
+    products.push(product)
+    res.send(product)
+})
 
 app.get("/", (req,res) =>{
     res.send("hello wolverine")
