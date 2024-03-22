5c96a09c3ac3db8594474f44b317cde7a0e19501
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2024-03-22 20:01:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -11,13 +11,20 @@ app.use(express.json())
 app.use("/api/products", products)
 app.use("/", home)
 
-mongoose.connect("mongodb+srv://bedirhansigirci:13579@cluster0.yhin6f0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    .then(() => {
-        console.log("mongodb bağlantısı kuruldu")
-    })
-    .catch((err) => {
-        console.log(err)
-    })
+const username = "bedirhansigirci"
+const password = "13579"
+const database = "ShopDb";
+
+    (async () => {
+        try {
+            await mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.yhin6f0.mongodb.net/${database}?retryWrites=true&w=majority&appName=Cluster0`)
+            console.log("mongodb bağlantısı kuruldu")
+        }
+        catch (err) {
+            console.log(err)
+        }
+
+})()
 
 const productSchema = mongoose.Schema({
     name: String,
@@ -32,6 +39,24 @@ const productSchema = mongoose.Schema({
 })
 
 const Product = mongoose.model("Product", productSchema) //model
+const prd = new Product({
+    name: "iPhone 14 Pro",
+    price: 60.000,
+    description: "İyi Telefon",
+    imageUrl: "1.jpeg",
+    isActive: true
+})
+async function saveProduct() {
+    try {
+        await prd.save()
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+saveProduct()
+
 
 app.listen(3000, () => {
     console.log("listening on port 3000")
