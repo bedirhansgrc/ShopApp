0ae1b0714206103e893994dff365c5afe20fdd4a
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2024-05-09 12:24:00 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,25 @@
 const express = require("express")
 const router = express.Router()
-const {User , validateLogin, validateRegister } = require("../models/user")
+const { User, validateLogin, validateRegister } = require("../models/user")
 const bcrypt = require("bcrypt")
+const auth = require("../middleware/auth")
+const isAdmin = require("../middleware/isAdmin")
 
-
-router.get("/" , async (req,res) => {
-    res.send()
+router.get("/",[auth, isAdmin], async (req, res) => {
+    const user = await User.find()
+    res.send(user)
 })
 
-router.post("/create" , async (req,res) => {
+router.post("/create", async (req, res) => {
     const { error } = validateRegister(req.body)
 
-    if(error){
+    if (error) {
         return res.status(400).send(error.details[0].message)
     }
-    
-    let user = await User.findOne({email: req.body.email})
 
-    if(user){
+    let user = await User.findOne({ email: req.body.email })
+
+    if (user) {
         return res.status(400).send("bu mail adresiyle bir kullanıcı mevcut.")
     }
 
@@ -39,17 +41,17 @@ router.post("/create" , async (req,res) => {
 router.post("/auth", async (req, res) => {
     const { error } = validateLogin(req.body)
 
-    if(error){
+    if (error) {
         return res.status(400).send(error.details[0].message)
     }
 
-    let user = await User.findOne({email: req.body.email})
-    if(!user){
+    let user = await User.findOne({ email: req.body.email })
+    if (!user) {
         return res.status(400).send("hatalı email veya parola")
     }
 
     const isSuccess = await bcrypt.compare(req.body.password, user.password)
-    if(!isSuccess){
+    if (!isSuccess) {
         return res.status(400).send("hatalı email veya parola")
     }
 
